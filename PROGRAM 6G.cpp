98a7e3e7ca9610fb38384e5acd19838776ac05fb
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-07-27 15:26:54 +0530

    Create PROGRAM 6G.cpp

---
+++ PROGRAM 6G.cpp
@@ -0,0 +1,30 @@
+// NAME: YASH SHRIVASTAVA
+// PRN: 24070123137
+// BATCH: E&TC-B2
+// TITLE: SIMPLE PYRAMID (EXPERIMENT: 6G)
+
+
+// CODE: 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, j;
+    int n=5;
+        for (i=1; i<=n; i++) {
+            cout<< endl; 
+            for(j=1; j<=i; j++) 
+            {cout<<"* ";
+            }
+        }
+        return 0;
+}
+
+// OUTPUT:
+
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * * 
