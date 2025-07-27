85ac6b29a720347bebfb37db7d685c7733cecdeb
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-07-27 15:20:32 +0530

    Create PROGRAM 6C.cpp

---
+++ PROGRAM 6C.cpp
@@ -0,0 +1,41 @@
+// NAME: YASH SHRIVASTAVA
+// PRN: 24070123137
+// BATCH: E&TC-B2
+// TITLE: TO PRINT NUMBERS TILL 19 (EXPERIMENT: 6C)
+
+
+// CODE: 
+
+#include <iostream>
+using namespace std;
+int main() {
+    int i=0;
+    while(i<20) {
+        cout<<i<<endl;
+        i++;
+    }
+    return 0;
+}
+
+// OUTPUT:
+
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+// 11
+// 12
+// 13
+// 14
+// 15
+// 16
+// 17
+// 18
+// 19
