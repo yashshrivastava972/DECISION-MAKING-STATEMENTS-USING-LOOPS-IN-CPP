8bf8ece2fa920181a33608a8b798dc1df8836df8
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-07-27 15:13:26 +0530

    Create PROGRAM 6A.cpp

---
+++ PROGRAM 6A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    for(int i=0; i<=10; i++){
+        if (i%2==0){
+           cout << i << "\n";
+        }
+    }
+    return 0;
+}
+
+// OUTPUT:
+
+// 0
+// 2
+// 4
+// 6
+// 8
+// 10
