0d3c465c96d1883556c28afd5858c0d00729aca7
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-07-27 15:18:56 +0530

    Create PROGRAM 6B.cpp

---
+++ PROGRAM 6B.cpp
@@ -0,0 +1,24 @@
+// NAME: YASH SHRIVASTAVA
+// PRN: 24070123137
+// BATCH: E&TC-B2
+// TITLE: USE A FOR LOOP TO PRINT "SIT" 5 TIMES (EXPERIMENT: 6B)
+
+
+// CODE:
+
+#include <iostream>
+using namespace std;
+int main() {
+    for(int i=1; i<= 5; i++) {
+           cout << "SIT" << endl;
+        }
+    return 0;
+}
+
+// OUTPUT:
+
+// SIT
+// SIT
+// SIT
+// SIT
+// SIT
